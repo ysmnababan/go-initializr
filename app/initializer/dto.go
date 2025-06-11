bd1ce49e9e21817a630faf3eeb427db8b3e6123b
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-06-11 23:33:12 +0700

    refactor BasicConfigRequest to use common types for DB and framework

--- app/initializer/dto.go
+++ app/initializer/dto.go
@@ -1,15 +1,14 @@
 package initializer
 
-type DBType string
-type FrameWorkType string
+import "go-initializr/app/common"
 
 type BasicConfigRequest struct {
-	ProjectName string        `json:"project_name" validate:"required"`
-	JWT         bool          `json:"jwt"`
-	Swagger     bool          `json:"swagger"`
-	Redis       bool          `json:"redis"`
-	Validator   bool          `json:"validator"`
-	DB          DBType        `json:"db" validate:"db-type"`
-	FrameWork   FrameWorkType `json:"framework" validate:"framework"`
-	ModInit     bool          `json:"mod_init"`
+	ProjectName string               `json:"project_name" validate:"required"`
+	JWT         bool                 `json:"jwt"`
+	Swagger     bool                 `json:"swagger"`
+	Redis       bool                 `json:"redis"`
+	Validator   bool                 `json:"validator"`
+	DB          common.DBType        `json:"db" validate:"db-type"`
+	FrameWork   common.FrameWorkType `json:"framework" validate:"framework"`
+	ModInit     bool                 `json:"mod_init"`
 }
