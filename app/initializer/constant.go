30191dc05ba885e9ad6995a56355ac0e08bd8365
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-06-11 23:34:10 +0700

    refactor constant.go to define template folder paths and registry

--- app/initializer/constant.go
+++ app/initializer/constant.go
@@ -1,10 +1,36 @@
 package initializer
 
-const (
-	DB_POSTGRES  DBType = "postgres"
-	DB_MYSQL     DBType = "mysql"
-	DB_SQLSERVER DBType = "sql_server"
+const TEMPLATE_FOLDER_PATH string = "./../template"
+const FOLDER_STRUCTURE_PATH string = "./../template/folder-structure.yaml"
+const GENERATED_ROOT_FOLDER = "generated"
 
-	FRAMEWORK_ECHO FrameWorkType = "echo"
-	FRAMEWORK_GIN  FrameWorkType = "gin"
-)
+var TEMPLATE_REGISTRY = map[string]string{
+	".env":                "env.tmpl",
+	"env.go":              "util_env.tmpl",
+	"stringutils.go":      "stringutils.tmpl",
+	"config_models.go":    "config_models.tmpl",
+	"config.go":           "config.tmpl",
+	"main.go":             "main.tmpl",
+	"database.go":         "database.tmpl",
+	"factory.go":          "factory.tmpl",
+	"redis.go":            "redis.tmpl",
+	"middleware.go":       "middleware.tmpl",
+	"auth.go":             "auth.tmpl",
+	"response_model.go":   "response_model.tmpl",
+	"error_handler.go":    "error_handler.tmpl",
+	"success_handler.go":  "success_handler.tmpl",
+	"pagination.go":       "pagination.tmpl",
+	"server_routes.go":    "server_routes.tmpl",
+	".gitignore":          "gitignore.tmpl",
+	"README.md":           "readme.tmpl",
+	"controller.go":       "controller.tmpl",
+	"service.go":          "service.tmpl",
+	"repository.go":       "repository.tmpl",
+	"model.go":            "model.tmpl",
+	"dto.go":              "dto.tmpl",
+	"routes.go":           "routes.tmpl",
+	"entity.go":           "entity.tmpl",
+	"validator.go":        "validator.tmpl",
+	"custom_validator.go": "custom_validator.tmpl",
+	"jwt.go":              "token.tmpl",
+}
