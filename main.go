70bc93d185ba4a954ad28f652a3cb134870bfab6
ysmnababan <ysmnababan@students.hacktiv8.ac.id>
2025-06-11 23:34:34 +0700

    refactor main.go to improve folder creation logic and code formatting

--- main.go
+++ main.go
@@ -4,19 +4,26 @@ import (
 	"go-initializr/app/initializer"
 	"go-initializr/app/pkg"
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
+	if err := os.MkdirAll(initializer.GENERATED_ROOT_FOLDER, os.ModePerm); err != nil {
+		panic(err)
+	}
 	e := echo.New()
 	e.Validator = pkg.NewCustomValidator()
+
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
 	version1 := e.Group("/v1")
 	handler := initializer.NewHandler()
 	version1.POST("/initialize", handler.InitializeBoilerplate)
 	version1.GET("/initialize/download/:id", handler.DownloadFolder)
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
